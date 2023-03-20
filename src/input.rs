e9cf97cd14620549fdf1b65741c6d85a23b4a7f7
squoop <the.sam.mcg@protonmail.com>
2023-03-20 19:05:11 +0000

    Refactor input.

--- src/input.rs
+++ src/input.rs
@@ -1,16 +1,30 @@
 use std::io::{Read, Write};
 use std::{io, thread};
 
-use crossbeam_channel::Receiver;
+use crossbeam_channel::{Receiver, TryRecvError};
 
-struct PollingStdin {
+pub struct PollingStdin {
     recv: Receiver<io::Result<Bytes>>,
 }
 
 #[derive(Default)]
-struct Bytes {
+pub struct Bytes {
     len: usize,
     buf: [u8; 32],
+}
+
+impl Bytes {
+    pub fn as_slice(&self) -> &[u8] {
+        &self.buf[..self.len]
+    }
+
+    pub fn len(&self) -> usize {
+        self.len
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.len == 0
+    }
 }
 
 impl PollingStdin {
@@ -41,36 +55,24 @@ impl PollingStdin {
         Self { recv }
     }
 
-    pub fn read_while_available(&mut self, mut dest: impl Write) -> io::Result<usize> {
-        let mut bytes_written = 0;
-
-        for res in self.recv.try_iter() {
-            let bytes = res?;
-
-            if bytes.len == 0 {
-                break;
-            }
-
-            dest.write_all(&bytes.buf[..bytes.len])?;
-            bytes_written += bytes.len;
+    pub fn read_while_available(&self) -> io::Result<Option<Bytes>> {
+        match self.recv.try_recv() {
+            Ok(bytes) => bytes.map(Some),
+            Err(TryRecvError::Empty) => Ok(None),
+            Err(TryRecvError::Disconnected) => panic!("sender disconnected"),
         }
-
-        Ok(bytes_written)
     }
 }
 
 #[test]
 fn foo() {
     let _term = crate::term::RawTermGuard::new(libc::STDIN_FILENO);
-
-    let mut stdin = PollingStdin::new();
+    let stdin = PollingStdin::new();
 
     loop {
-        let mut buf = vec![];
-        stdin.read_while_available(&mut buf).unwrap();
-
-        if !buf.is_empty() {
-            print!("{buf:?}\r\n");
-        }
+        let Some(bytes) = stdin.read_while_available().unwrap() else {
+            continue;
+        };
+        print!("{:?}\r\n", bytes.as_slice());
     }
 }
