c8b7c211c0b76be2e631a0860b443ed7ef6586d0
squoop <the.sam.mcg@protonmail.com>
2023-03-28 17:53:57 +0100

    Clean up `main.rs`.

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,10 @@
 use std::time::Duration;
 
-
 use text_editor::widget::{App, InputPrinter};
 
 fn main() {
-    let app = App::new(InputPrinter::default(), Duration::from_millis(17)).expect("couldn't create app");
+    let refresh_rate = Duration::from_millis(17);
+
+    let app = App::new(InputPrinter::default(), refresh_rate).expect("couldn't create app");
     app.run().expect("IO error");
-
-    // let mut term = Term::new(libc::STDIN_FILENO).expect("couldn't enter raw mode");
-
-    // loop {
-    //     let size = term.size().expect("couldn't get terminal size");
-    //     if size.0 == 0 || size.1 == 0 {
-    //         continue;
-    //     }
-
-    //     let mut buffer = Buffer::filled(size.0, size.1, Cell::default());
-
-    //     let c = 'X';
-
-    //     buffer[[0, 0]].c = c;
-    //     buffer[[0, size.1 - 1]].c = c;
-    //     buffer[[size.0 - 1, 0]].c = c;
-    //     buffer[[size.0 - 1, size.1 - 1]].c = c;
-
-    //     term.render_buffer(&buffer).expect("couldn't render buffer");
-
-    //     std::thread::sleep(Duration::from_millis(17));
-    // }
 }
